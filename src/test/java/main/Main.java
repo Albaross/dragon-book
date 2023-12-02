e61bbafc34a55b77c8cb7eecc5b9a7e423b8bba7
manuel.feilen <manuel.feilen@de.rhenus.com>
2023-12-02 17:05:58 +0100

    feed Lexer from test file

--- src/test/java/main/Main.java
+++ src/test/java/main/Main.java
@@ -7,9 +7,11 @@ import parser.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Lexer lex = new Lexer();
-        Parser parse = new Parser(lex);
-        parse.program();
-        System.out.write('\n');
+        try (final var in = new FileInputStream("src/test/resources/test")) {
+            Lexer lex = new Lexer(in);
+            Parser parse = new Parser(lex);
+            parse.program();
+            System.out.write('\n');
+        }
     }
 }
