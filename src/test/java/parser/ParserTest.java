06b10d4e4e496f4ba62a197e262b56efab6d919a
manuel.feilen <manuel.feilen@de.rhenus.com>
2023-12-02 17:49:15 +0100

    rename file to ParserTest

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,11 +1,10 @@
-package main;
+package parser;
 
 import java.io.*;
 
 import lexer.*;
-import parser.*;
 
-public class Main {
+public class ParserTest {
     public static void main(String[] args) throws IOException {
         try (final var in = new FileInputStream("src/test/resources/test")) {
             Lexer lex = new Lexer(in);
