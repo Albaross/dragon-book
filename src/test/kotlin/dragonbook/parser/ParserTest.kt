8b6dda459be82b53083d57e8aaf39988596e7dfd
Manuel Feilen <manuel.feilen@de.rhenus.com>
2023-12-13 09:56:03 +0100

    moved test file to new package

---
+++ src/test/kotlin/dragonbook/parser/ParserTest.kt
@@ -0,0 +1,25 @@
+package dragonbook.parser
+
+import dragonbook.gen.Generator
+import dragonbook.lexer.Lexer
+import dragonbook.test.EXPECTED_GENERATOR_OUTPUT
+import org.junit.jupiter.api.Test
+import java.io.FileInputStream
+import kotlin.test.assertEquals
+
+class ParserTest {
+
+    @Test
+    fun `code is generated correctly`() {
+        FileInputStream("src/test/resources/test").use {
+            val lex = Lexer(it)
+            val parse = Parser(lex)
+            val gen = Generator(parse)
+
+            val result = gen.gen()
+            result.forEach(::println)
+            assertEquals(EXPECTED_GENERATOR_OUTPUT, result)
+        }
+    }
+}
+
